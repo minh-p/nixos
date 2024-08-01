427a3052f507dde61f9fee1d6d7c5530c4d00214
minh-p <minhisverynormal@gmail.com>
2024-08-01 13:09:39 -0700

    Catppuccin mako

--- modules/home-manager/wm/mako.nix
+++ modules/home-manager/wm/mako.nix
@@ -8,11 +8,13 @@
       enable = true;
       sort = "-time";
       layer = "overlay";
-      backgroundColor = "#285577";
+      backgroundColor = "#1a1b26";
       width = 300;
       height = 110;
       borderSize = 2;
-      borderColor = "#000000";
+      borderColor = "#c0caf5";
+      progressColor = "over #302D41";
+      textColor = "#c0caf5";
       borderRadius = 15;
       icons = true;
       maxIconSize = 64;
@@ -20,16 +22,6 @@
       ignoreTimeout = true;
       font = "DejaVu Sans Mono 14";
       extraConfig = ''
-        [urgency=low]
-        border-color=#cccccc
-
-        [urgency=normal]
-        border-color=#d08770
-
-        [urgency=high]
-        border-color=#bf616a
-        default-timeout=0
-
         [category=mpd]
         default-timeout=2000
         group-by=category
