4869735bab30bd62e9b943856e8c9bd82fded90a
minh-p <minhisverynormal@gmail.com>
2024-08-04 19:38:53 -0700

    Silent Boot option

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -13,30 +13,49 @@
       ../../modules/nixos/stylix
     ];
 
-  boot.loader = {
-    efi = {
-      canTouchEfiVariables = true;
+  boot = {
+    plymouth.enable = true;
+    loader = {
+      efi = {
+        canTouchEfiVariables = true;
+      };
+      grub = {
+        enable = true;
+        useOSProber = true;
+        copyKernels = true;
+        efiSupport = true;
+        fsIdentifier = "label";
+        #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
+        device = "nodev";
+        extraEntries = ''
+            menuentry "Reboot" {
+                reboot
+            }
+            menuentry "Poweroff" {
+                halt
+            }
+        '';
+      };
     };
-    grub = {
-      enable = true;
-      useOSProber = true;
-      copyKernels = true;
-      efiSupport = true;
-      fsIdentifier = "label";
-      #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
-      device = "nodev";
-      extraEntries = ''
-          menuentry "Reboot" {
-              reboot
-          }
-          menuentry "Poweroff" {
-              halt
-          }
-      '';
-    };
-  };
 
-  boot.plymouth.enable = true;
+    # Enable "Silent Boot"
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    loader.timeout = 0;
+  };
 
   networking.hostName = "Aurelius"; # Define your hostname.
   # Pick only one of the below networking options.
