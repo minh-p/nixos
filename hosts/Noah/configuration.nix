d7f624c652f46d209a2796a1971f0bfdcf8bde7b
minh-p <minhisverynormal@gmail.com>
2025-10-02 00:21:11 -0700

    add: tree tree tree!

--- hosts/Noah/configuration.nix
+++ hosts/Noah/configuration.nix
@@ -213,6 +213,7 @@
   environment.systemPackages = with pkgs; [
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     zsh
+    tree
     ventoy
     tmux
     pulseaudio
