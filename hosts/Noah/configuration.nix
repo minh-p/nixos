4988878fb9f83812df907cea0f1f2dc28ef47965
minh-p <minhisverynormal@gmail.com>
2025-10-02 00:05:32 -0700

    replace: autocpu-freq with tlp for laptop Noah.

--- hosts/Noah/configuration.nix
+++ hosts/Noah/configuration.nix
@@ -18,7 +18,7 @@
     loader = {
       efi = {
         canTouchEfiVariables = true;
-	efiSysMountPoint = "/boot/EFI";
+        efiSysMountPoint = "/boot/EFI";
       };
       grub = {
         enable = true;
@@ -57,7 +57,6 @@
     # It will just not appear on screen unless a key is pressed
     loader.timeout = 5;
   };
-
 
   hardware.enableAllFirmware = true;
 
@@ -104,6 +103,28 @@
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  powerManagement.enable = true;
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
+
+      #Optional helps save long term battery health
+      START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
+      STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+    };
+  };
 
   services.gvfs.enable = true;
   services.udisks2.enable = true;
@@ -152,9 +173,7 @@
 
   # Allow unfree
   nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.permittedInsecurePackages = [
-    "ventoy-1.1.05"
-  ];
+  nixpkgs.config.permittedInsecurePackages = [ "ventoy-1.1.05" ];
 
   # Allow experimental nix command
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -202,7 +221,6 @@
     clang
     clang-tools
     fd
-    auto-cpufreq
     wget
     libgcc
     coreutils
@@ -248,8 +266,6 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
-
-  services.auto-cpufreq.enable = true;
 
   services.gnome.gnome-keyring.enable = true;
   services.displayManager.sessionPackages = [ pkgs.sway ];
